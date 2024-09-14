ce4a4190d25dbb1d5fe414f42b75d919cc3e01c1
Sving1024 <piano5432@outlook.com>
2024-09-14 21:26:12 +0800

    更新类方法

--- src/pdf_luogu/luogu.py
+++ src/pdf_luogu/luogu.py
@@ -14,27 +14,26 @@ class Problem:
     __BASE_URL='https://www.luogu.com.cn/problem/'
     id=''
     data=None
-    __html_cache=None
+    markdown = None
 
     def __init__(self,_id) -> None:
         self.id=_id
 
     def fetchResources(self) -> httpx.Response :
         """将请求存储到 __html_cache 中"""
-        self.__html_cache=httpx.get(self.__BASE_URL+self.id,params={'_contentOnly' : ''})
-        return self.__html_cache
-    def parseRespose(self):
+        raw_resources=httpx.get(self.__BASE_URL+self.id,params={'_contentOnly' : ''})
         """解析请求到的 json"""
-        tmp=json.loads(self.__html_cache.text)
-        self.data = tmp['currentData']['problem']
+        rescoures=json.loads(raw_resources.text)
+        self.data = rescoures['currentData']['problem']
         return self.data
-
+    
+    def diffculty(self) -> int:
+        return self.data['diffculty']
 
 class Training:
     __BASE_URL='https://www.luogu.com.cn/training/'
     id=''
     data=None
-    __html_cache=None
 
     def __init__(self,_id) -> None:
         self.id=_id
@@ -42,10 +41,7 @@ class Training:
     def fetchResources(self) -> httpx.Response:
         if self.__html_cache != None:
             return self.__html_cache
-        self.__html_cache=httpx.get(self.__BASE_URL+self.id,params={'_contentOnly' : ''})
-        return self.__html_cache
-
-    def parseRespose(self):
-        tmp = json.loads(self.__html_cache.text)
-        self.data=tmp['currentData']['training']
+        raw_resources=httpx.get(self.__BASE_URL+self.id,params={'_contentOnly' : ''})
+        rescoures = json.loads(raw_resources.text)
+        self.data=rescoures['currentData']['training']
         return self.data
